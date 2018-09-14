6d18eaafb5456b6629d9c2577d119e381062817d
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-14 10:58:31 -0600

    vent
    vent

--- application/controllers/ventas.php
+++ application/controllers/ventas.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Ventas exteds CI_Controller{
+class Ventas extends CI_Controller{
 
 	public function index()
 	{
-		$this->load->helper('url');
+		
 		$this->load->view('frmVenta');
 	}
 }
