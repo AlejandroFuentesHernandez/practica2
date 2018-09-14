e544111064a8f68351772da7479d367aaf706d35
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-14 11:33:01 -0600

    j
    js

--- application/controllers/ventas.php
+++ application/controllers/ventas.php
@@ -9,13 +9,42 @@ class Ventas extends CI_Controller{
 		$this->load->helper('url');
 		$this->load->view('error');
 	}
-	
+
 	public function index()
 	{
 		$this->load->helper('url');
-		$this->load->view('frmVentas');
+		$this->load->view('frmVenta');
 	}
 
-	
+	/*public function realizar()
+	{
+		foreach($cliente as $item){
+			echo '<option value="item[id_cliente]".$item["nombre_cliente"].</option>';
+		}
+
+
+			Input 1--- nombre_cliente
+2.       Input 2--- fecha_venta
+3.       Input 3--- nombre_producto
+4.       Input 4--- precio_unitario_producto
+5.       Input 5--- cantidad
+6.       Input 6--- total
+
+
+
+	}*/
+	public function cargarcliente()
+	{
+		$this->load->model('');
+		$resultado=$this->DM->llenarcliente();
+		foreach ($resultado as $item) {
+			echo '<option value="'.$item['id_cliente'].'">'.$item['cliente'].'</option>';
+		}
+	}
+
+	public function cargarproducto()
+	{
+
+	}
 }
 ?>
