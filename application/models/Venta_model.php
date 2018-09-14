06ad7f137974d250bfce6c1eaaf2ae51bc63e43a
@EsaSanchez <samuel.ramirez110590@gmail.com>
2018-09-14 21:19:51 +0200

    model
    Modelo terminado

---
+++ application/models/Venta_model.php
@@ -0,0 +1,28 @@
+<?php
+//Creamos la Insercion de datos de una tabla
+class Venta_model extends CI_Model
+{ //Inicio de consulta
+    
+    public function nuevoVenta()
+    {
+        $this->load->database();
+        $this->db->trans_begin();//Inicio de transacción//
+        
+        $this->db->query("INSERT INTO tab_venta(id_cliente,fecha_venta, total_productos_venta,total_venta) VALUES("$id_cliente", "$fecha_venta", "$total_productos_venta", "$total_venta")");
+
+        $id_venta=$this->db->insert_id();//Funciona para recuperar el ultimo id autoincrement ingresado//
+
+         $this->db->query("INSERT INTO tab_venta_producto(id_venta, id_producto) VALUES("$id_venta", "$id_producto")");
+         if($this->db->trans_status()===false;
+         {
+            $this->db->trans_rollback();
+            return 0;
+         }else
+         {
+            $this->db->trans_commit();
+            return 1;
+         }
+        
+
+    }
+}//Fin de Insercion de datos
