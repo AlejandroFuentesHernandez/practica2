5355ed8ad7b6110edc0e740d1fb2871f997449bd
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-26 11:17:46 -0600

    box
    box en proceso

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # practica2
 jquery
+
+//"serverSide": true,//{data": ["nombre_prod"]}; 
+<!-- 	<tbody id="showdata">
+						
+					</tbody>
+ -->
+//mostrarUsuarios();
+/*function mostrarUsuarios()
+	{
+		
+		$.ajax({
+			"ajax":,//hereda todas las opcione disponibles por jquery
+			"url":"<?php echo base_url() ?>Producto/mostrar"
+			"dataSrc":""//para alterar que dtaos leera el json
+			"data":{
+			"user_id": #showdata
+		}
+
+		}),
+			
+	}
+	$.ajax({
+			type: 'ajax',
+			url: '<?php echo base_url() ?>Producto/mostrar',
+			async:false,
+			dataType: 'json',
+			success: function(data){
+				var html = '';
+				var i;
+
+				for(i=0;i<data.length;i++)
+				{ 
+					html += '<tr>'+
+							'<td>'+data[i].nombre_producto+'</td>'+
+							'<td>'+data[i].nombre_tipo+'<td>'+
+							'<td>'+data[i].descripcion_producto+'</td>'+
+							'<td>'+data[i].nombre_proveedor+'</td>'+
+							'<td>'+data[i].stock_minimo_producto+'</td>'+
+							'<td>'+data[i].existencias_producto+'</td>'+
+							'<td>'+data[i].estado_producto+'</td>'+
+							'<td>'+data[i].fecha_caducidad_producto+'</td>'+
+							'<td>'+data[i].precio_unitario_producto+'</td>'
+							//'<td>'<a href="javascript:;" class="btn btn-success item-view" data="'+data[i].id_producto'"><i class=""></i></a>
+							//'</td>'+
+							//'<td>'<a href="javascript:;" class="btn btn-info item-edit" data="'+data[i].id_producto'"><i class=""></i></a>'</td>'+
+								//'<td>'<a href="javascript:;" class="btn btn-danger item-delete" data="'+data[i].id_producto'"><i class=""></i></a>'</td>'
+						'</tr>';
+				}
+				$('#showdata').html(html);
+			},
+			error: function()
+			{
+				alert('No se pudo obtener los datos');
+			}
+		});*/
--- application/views/producto_view.php
+++ application/views/producto_view.php
@@ -10,7 +10,7 @@ existencias_producto
 estado_producto
 fecha_caducidad_producto
 precio_unitario_producto
-
+<!-- visualizacion css-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 */
@@ -25,28 +25,26 @@ precio_unitario_producto
 	<meta name="viewport" content="width=device-witdth, inicial-scale="1">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	
 
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">	
+<!-- jquery min -->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<!-- data table css -->
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
-
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-
-
-
+<!-- ajax--->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<!-- max -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
 </head>
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="well well-sm">
-		  <div class="card bg-light text-dark"><!-- car titulo color blanco texto en negro-->
+<body style="background-color: #99ffd6 ">
+	<div class="container" >
+		<div class="row" align="center">
+			<div class="card" align="center">
+				<div class="center-block"><!-- center-->
+			<div class="well well-sm">     
+		  <div class="card bg-light text-dark"><!-- car titulo color blanco texto en negro   style="background-color: #ffccff"-->
 			<div class="card-header">
 				<!-- car titulo  para modulo inventario-->
 				<h4 class="card-title">Modulo del inventario</h4>
@@ -74,11 +72,14 @@ precio_unitario_producto
 				</table>
 				</div><!-- tabla responsive-->
 			</div><!-- card body-->
-
 			<div class="card-footer"></div>
+			</div><!-- center-->
+			
 		</div><!-- texto fondo blanco-->
 		</div><!--card bg encabezado -->
 		</div><!--well pegado borde gris y borde redondeado   -->
+		
+		</div><!-- card -->
 		</div><!-- classrow-->
 	</div><!-- container-->
 	
